f0a4ed3bfa6c75959b24a13aa8a4154dfbc8e9a4
Revel Roestorf <revel.roestorf@gmail.com>
2018-05-29 19:18:36 +0200

    omit challange

---
+++ challenges/omit.js
@@ -0,0 +1,31 @@
+/*
+Omit
+Return a copy of the object, filtered to omit the blacklisted keys
+Example:
+omit({name: 'moe', age: 50, userid: 'moe1'}, 'name', 'age');
+=> {userid: 'moe1'}
+Hint: Given there's an indefinite number of arguments, we'll need to loop
+through them. Look up the "rest parameter" syntax.
+*/
+
+// Your code here!
+const omit = () => {
+}
+
+// Check your solution by running these tests: mocha *this_filename*
+const assert = require('assert');
+
+describe('Omit', () => {
+  const list = {a: 1, b: 2, c: 3};
+
+  it('can omit a single property', () => {
+    const result = omit(list, 'a');
+    const expected = {b: 2, c: 3};
+    assert.deepEqual(result, expected);
+  })
+  it('can omit multiple properties', () => {
+    const result = omit(list, 'a', 'c');
+    const expected = {b: 2};
+    assert.deepEqual(result, expected);
+  })
+});
